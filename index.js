f07bfdd9c002d90f9f2f2f347cb2cd251404aae9
Rajnish Kumar <rajnish.kumar@rotary.org>
2019-02-07 16:43:25 +0530

    Resetting master

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var app = express();
-var morgan = require('morgan'); // loads the piece of middleware for logging
-var favicon = require('serve-favicon'); // loads the piece of middleware for the favicon
 
 const PORT = process.env.NODE || 3000;
-
-app.use(morgan('combined')) // loads the piece of middleware for logging
-.use(express.static(__dirname + '/public')) // Specifies that the /public folder includes static files (basic piece of middleware loaded)
-.use(favicon(__dirname + '/public/favicon.ico')) // Activates the favicon specified
-.use(function(req, res){ // finally answers
-    res.send('Hello');
-});
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
@@ -20,15 +11,6 @@ app.get('/', function(req, res) {
 app.get('/basement', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('You\'re in the wine cellar. Those bottles are mine!');
-});
-
-app.get('/floor/:floornum/bedroom', function(req, res) {
-    res.render('bedroom.ejs', {floor: req.params.floornum});
-});
-
-app.get('/count/:number', function(req, res) {
-    var names = ['Robert', 'Jack', 'David'];
-    res.render('page.ejs', {counter: req.params.number, names: names});
 });
 
 // ...All the route management code (app. get) is above
